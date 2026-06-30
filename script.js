391310e45f69feb43ba04db5366498ddcb08ec6b
Shubham <xdark5071@gmail.com>
2026-06-30 11:05:44 +0530

    Added local storage

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const slider = document.getElementById("slider");
 const value = document.getElementById("value");
 
+// Load saved value
+const savedValue = localStorage.getItem("sliderValue");
+
+if (savedValue !== null) {
+    slider.value = savedValue;
+    value.textContent = savedValue + "%";
+} else {
+    value.textContent = slider.value + "%";
+}
+
+// Update and save value
 slider.oninput = function () {
     value.textContent = this.value + "%";
+    localStorage.setItem("sliderValue", this.value);
 };
